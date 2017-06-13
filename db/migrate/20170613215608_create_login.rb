13f51912ee81a941e69b3c9f16995ca052c3c212
Samuel Pedraza <samdpedraza@gmail.com>
2017-06-13 14:59:04 -0700

    raked db, added link as a comment

--- db/migrate/20170613215608_create_login.rb
+++ db/migrate/20170613215608_create_login.rb
@@ -1,4 +1,14 @@
 class CreateLogin < ActiveRecord::Migration[5.1]
-  def change
+  def up
+  	create_table :login do |t|
+  		t.string :name
+  		t.string :password
+  	end
+  end
+
+  def down
+  	drop_table :login
   end
 end
+
+#https://samuelstern.wordpress.com/2012/11/28/making-a-simple-database-driven-website-with-sinatra-and-heroku/
